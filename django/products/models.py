26e3818eeec2a221b17ca756339520535a65e60f
PrantaChakraborty <cbpranta@gmail.com>
2022-05-31 16:02:47 +0600

    create models

--- django/products/models.py
+++ django/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    likes = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    pass
