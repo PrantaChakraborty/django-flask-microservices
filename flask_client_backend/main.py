e8f7dfc1ba41f19232398e86d5ca2c7a5279c552
PrantaChakraborty <cbpranta@gmail.com>
2022-06-01 18:01:10 +0600

    setup connection to postgresql

--- flask_client_backend/main.py
+++ flask_client_backend/main.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+
+# db table
+class PostClient(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+
 
 @app.route('/')
 def hello_world():
